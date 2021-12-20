e630a787c05ef9c41af807353b84fa4ddf478be5
kross <cristemperatum@gmail.com>
2021-12-20 15:46:57 -0600

    ejercicio 5

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,35 @@
 
 // Source: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
 
-function simpleArraySum(ar: Array<number>): number {
-  let suma = 0;
-  ar.forEach((element) => suma += element);
-  return suma;
-}
+function plusMinus(arr: number[]): void {
+  const longitud = arr.length
+  let positives = 0;
+  let negative = 0;
+  let neutrals = 0;
 
+  for (let i = 0; i < longitud; i++) {
+    if( arr[i] > 0){
+      positives++
+    }if( arr[i] < 0 ){
+      negative++
+    } else {
+      neutrals++
+    }
+  }
+  const propp = positives / longitud;
+  const propneg = negative / longitud;
+  const propneu = neutrals / longitud;
+
+  console.log((propp).toFixed(6));
+  console.log((propneg).toFixed(6));
+  console.log((propneu).toFixed(6));
+}
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const testArray = [1, 2, 3, 4, 10, 11];
-  const result = simpleArraySum(testArray);
+  const a = [-4, 3, -9, 0, 4, 1]
+  const result = plusMinus(a);
   console.log('Result: ', result);
 }
 
 main();
+
